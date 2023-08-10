8492db35e6657e74c28698bc9caa7faa974a6964
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-10 12:32:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+let protect = document.getElementById("but");
+let sign = document.getElementById("form").style.display = "none";
+
+protect.addEventListener('click', () => {
+    document.getElementById("item").style.display = "none";
+    document.getElementById("form").style.display = "block";
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = JSON.parse(window.Telegram.WebApp.initDataUnsafe.data);
+
+usercard.appendChild(p);
