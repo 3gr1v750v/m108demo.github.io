d3218c3d722da3130572e9b6517fff385150b096
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-10 14:48:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = new URLSearchParams(window.location.search).get('data');
+p.innerText = `${tg.platform}`;
 
 usercard.appendChild(p);
