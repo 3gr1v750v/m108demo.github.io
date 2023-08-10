132fedd8ce7239d336d3a409e7eca81128a9416a
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-10 18:23:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ protect.addEventListener('click', () => {
     document.getElementById("form").style.display = "block";
 });
 
-let usercard = document.getElementById("usercard");
+// Get the current URL
+const currentUrl = window.location.href;
 
-let p = document.createElement("p");
+// Function to extract parameter value from URL
+function getParamValue(paramName) {
+    const urlParams = new URLSearchParams(currentUrl);
+    return urlParams.get(paramName);
+}
 
-p.innerText = `${tg.dataset}`;
+// Extract and display param1 value
+const param1Value = getParamValue('param1');
+const usercardElement = document.getElementById('usercard');
 
-usercard.appendChild(p);
+// Create a paragraph element to display the param1 value
+const paramValueElement = document.createElement('p');
+paramValueElement.textContent = `Param1 Value: ${param1Value}`;
+
+// Append the paragraph element to the usercard element
+usercardElement.appendChild(paramValueElement);
