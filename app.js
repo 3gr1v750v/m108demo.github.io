b0c746cbf8886f486620a4ff5d2b3f446f52b76e
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-10 12:38:38 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = JSON.parse(window.Telegram.WebApp.initDataUnsafe.data);
+p.innerText = window.Telegram.WebApp.initDataUnsafe.data;
 
 usercard.appendChild(p);
