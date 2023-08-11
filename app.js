12e6c4702d4508c79284181cf994d7c17d10b980
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-11 13:42:04 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Попытка принудительной очистки кэша при открытии сайта
 window.onload = function() {
   if (window.performance && window.performance.clearResourceTimings) {
     window.performance.clearResourceTimings();
@@ -17,33 +18,58 @@ window.onload = function() {
   }
 };
 
+// Вызов объекта телеграм WebApp
 let tg = window.Telegram.WebApp;
 
+// Раскрыть окно на полный экран мобильника
 tg.expand();
 
+// Объекты сайта
 let mainButton = document.getElementById("but");
 let signForm = document.getElementById("form");
 let signUpButton = document.getElementById('sign')
 signForm.style.display = "none";
 
+// Что произойдёт при нажатии на первую кнопку
 mainButton.addEventListener('click', () => {
     let container = document.querySelector(".container");
     container.style.display = "none";
     signForm.style.display = "block";
+    // Перехват имени и фамилии пользователя и подстановка в поле формы
     document.getElementById('user_name').value = `${tg.initDataUnsafe.user.first_name}` + " " + `${tg.initDataUnsafe.user.last_name}`;
 });
 
+// Что произойдёт при нажатии на вторую кнопку
 signUpButton.addEventListener('click', () => {
+    // Очищаем данные ошибок
+    document.getElementById("error").InnerText = '';
+
+    // Забираем значения
+    let name = document.getElementById('user_name').value;
+    let phone = document.getElementById('phone_number').value;
+
+    // Проверка введённых данных
+    if(phone.length < 1) {
+        document.getElementById("error").InnerText = "Please enter you phone";
+        return;
+    };
+
+    // Создаём массив данных из полученных данных
+    let data = {
+        name: name,
+        phone: phone
+    }
+
+    // Отправляем данные обратно в Telegram
+    tg.sendData(JSON.stringify(data));
+
+    // Закрываем сайт
     tg.close()
 });
 
+
+// Перехват 'param1' из параметров ссылки и подстановка в сайт
 const urlParams = new URLSearchParams(window.location.search);
-
-// Get the 'param1' value
 const param1 = urlParams.get('param1');
-
-// Select the existing <p> element with the class "alien_message"
 const alienMessageElement = document.querySelector('.alien_message');
-
-// Update the text content of the <p> element
 alienMessageElement.textContent = param1;
