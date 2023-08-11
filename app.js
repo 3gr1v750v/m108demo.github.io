cd19d4fb30ae5f07b22fc2d88a25a10f0d0eae9e
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-11 12:16:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let protect = document.getElementById("but");
-let sign = document.getElementById("form").style.display = "none";
+let protect = document.getElementById("btn");
+let signForm = document.getElementById("form").style.display = "none";
 
 protect.addEventListener('click', () => {
-    document.getElementById("item").style.display = "none";
-    document.getElementById("form").style.display = "block";
+    document.getElementById(".container").style.display = "none";
+    signForm.style.display = "block";
+    document.getElementById('user_name').value = tg.InitDataUnsafe.user.first_name + " " + tg.InitDataUnsafe.user.last_name
 });
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -15,16 +16,8 @@ const urlParams = new URLSearchParams(window.location.search);
 // Get the 'param1' value
 const param1 = urlParams.get('param1');
 
-// Select the <div> element
-const usercard = document.getElementById('usercard');
+// Select the existing <p> element with the class "alien_message"
+const alienMessageElement = document.querySelector('.alien_message');
 
-const p = document.createElement('p');
-
-// Create the text node for message
-const message = document.createTextNode(param1);
-
-// Add text node to <p> element
-p.appendChild(message);
-
-// Add <p> inside <div>
-usercard.appendChild(p);
+// Update the text content of the <p> element
+alienMessageElement.textContent = param1;
