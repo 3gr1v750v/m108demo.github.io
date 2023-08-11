b9f1081d8f25b3c4d03fecd2b823e3993ab7721a
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-11 12:57:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mainButton.addEventListener('click', () => {
     let container = document.querySelector(".container");
     container.style.display = "none";
     signForm.style.display = "block";
-    document.getElementById('user_name').value = `${tg.initDataUnsafe.user.first_name}`;
+    document.getElementById('user_name').value = `${tg.initDataUnsafe.user.first_name}` + " " + `${tg.initDataUnsafe.user.last_name}`;
 });
 
 const urlParams = new URLSearchParams(window.location.search);
