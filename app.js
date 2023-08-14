4e50310286e03dccd26b6bf81cc3a24f42ab8a64
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-14 14:12:01 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ let mainButton = document.getElementById("but");
 let signForm = document.getElementById("form");
 let signUpButton = document.getElementById('sign')
 signForm.style.display = "none";
+const botToken = "6559961659:AAHWQ_hoxUmnhwhUWIDcXxLJR6KwhyeYoDw";
+const chatId = tg.initDataUnsafe.user.id;
 
 //id сессии для отправки сообщений из webApp в телеграм
 let query_id = tg.initDataUnsafe.query_id;
@@ -52,7 +54,17 @@ signUpButton.addEventListener('click', () => {
     что 'Data from the "Посмотреть сообщение" button was transferred to the bot.'
     Но деталей, что это за сообщение - не показывает.
     */
-    tg.sendData(JSON.stringify(data));
+    // tg.sendData(JSON.stringify(data));
+
+    const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: "Завершение работы",
+        })};
 
 
     // Закрываем сайт
