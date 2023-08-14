bb31fcc4ac68a8246b01d37015f2b4dcadcb2cfa
Evgenii Grivtsov <egrivtsov@gmail.com>
2023-08-14 14:13:12 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -57,14 +57,15 @@ signUpButton.addEventListener('click', () => {
     // tg.sendData(JSON.stringify(data));
 
     const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            chat_id: chatId,
-            text: "Завершение работы",
-        })};
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        chat_id: chatId,
+        text: "Завершение работы",
+    })
+    });
 
 
     // Закрываем сайт
